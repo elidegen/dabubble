309f082c7f313d953bec912fcca2268a5cfa9dc0
elidegen <elidegen@icloud.com>
2023-11-22 16:47:09 +0100

    firebase installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"dabubble-c4b96","appId":"1:390532295885:web:ad6cd75380acc256d50d84","storageBucket":"dabubble-c4b96.appspot.com","apiKey":"AIzaSyAEu4ozPAp4fPV6zGVHoY_x9YT8wbZrMs4","authDomain":"dabubble-c4b96.firebaseapp.com","messagingSenderId":"390532295885"})),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
