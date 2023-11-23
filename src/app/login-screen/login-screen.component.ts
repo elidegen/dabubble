ac0c824f40b4920a3850812d8e23345eff2dd8a4
michaelwoelfel <woelfelm@t-online.de>
2023-11-23 19:57:21 +0100

    start-screen animation

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss']
 })
-export class LoginScreenComponent {
+export class LoginScreenComponent implements OnInit {
+animationIsFinished = false;
+
+
+  ngOnInit() {
+    this.hideContentAfterAnimation();
+
+  }
+
+
+ 
+
+
+
+  hideContentAfterAnimation() {
+    setTimeout(() => {
+   this.animationIsFinished = true;
+    }, 2500);
+     }
 
 }
+ 
+
+
+
