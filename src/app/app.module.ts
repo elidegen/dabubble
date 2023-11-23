4f41595c0fb713aa9309a2cabaaf25dc6db3e56d
elidegen <elidegen@icloud.com>
2023-11-23 08:59:14 +0100

    mat imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { HomeComponent } from './home/home.component';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
+
+
 
 
 @NgModule({
@@ -19,11 +42,30 @@ import { MatInputModule } from '@angular/material/input';
   ],
   imports: [
     BrowserModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRippleModule,
+    MatRadioModule,
     AppRoutingModule,
     MatInputModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"dabubble-c4b96","appId":"1:390532295885:web:ad6cd75380acc256d50d84","storageBucket":"dabubble-c4b96.appspot.com","apiKey":"AIzaSyAEu4ozPAp4fPV6zGVHoY_x9YT8wbZrMs4","authDomain":"dabubble-c4b96.firebaseapp.com","messagingSenderId":"390532295885"})),
+    provideFirebaseApp(() => initializeApp({ "projectId": "dabubble-c4b96", "appId": "1:390532295885:web:ad6cd75380acc256d50d84", "storageBucket": "dabubble-c4b96.appspot.com", "apiKey": "AIzaSyAEu4ozPAp4fPV6zGVHoY_x9YT8wbZrMs4", "authDomain": "dabubble-c4b96.firebaseapp.com", "messagingSenderId": "390532295885" })),
     provideFirestore(() => getFirestore()),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatSliderModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
