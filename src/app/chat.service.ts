c98af16ebdce37fe20a3dd386606c3b5ebe604ce
michaelwoelfel <woelfelm@t-online.de>
2023-11-29 13:38:54 +0100

    chatService subChannels

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,6 +1,5 @@
-import { Inject, Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
-import { collection, doc } from 'firebase/firestore';
+import { inject, Injectable } from '@angular/core';
+import { Firestore, collection, doc, collectionData, onSnapshot, addDoc, deleteDoc, updateDoc, } from '@angular/fire/firestore';
 import { Channel } from 'src/models/channel.class';
 import { Chat } from 'src/models/chat.class';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -9,9 +8,14 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ChatService {
+  firestore: Firestore = inject(Firestore);
+  allChannels: Channel[] = [];
+  unsubList;
   private _openChatSubject: BehaviorSubject<Channel | Chat | null> = new BehaviorSubject<Channel | Chat | null>(null);
-  firestore: Firestore = Inject(Firestore)
-  constructor() { }
+ 
+  constructor() {
+    this.unsubList = this.subChannelList();
+  }
 
   get openChat$(): Observable<Channel | Chat | null> {
     return this._openChatSubject.asObservable();
@@ -22,9 +26,33 @@ export class ChatService {
   }
 
 
+  ngOnInit() {
+   
+  }
 
-  
+ngOnDestroy() {
+this.unsubList();
+}
 
+
+
+
+subChannelList() {
+  return onSnapshot(this.getChannelsRef(), (list) => {
+    this.allChannels = [];
+    list.forEach(element  => {
+      this.allChannels.push(element.data() as Channel);
+    })
+  })
+}
+
+
+
+
+
+getChannelRef(colId: string) {
+  return collection(this.firestore, colId);
+}
 
   getChannelsRef() {
     return collection(this.firestore, 'channels');
