6af9485ecbd7fb7cd5d82850244d31ea10922c6e
Raphael Janka <135700986+RaphaelJanka@users.noreply.github.com>
2023-12-09 15:41:14 +0100

    firebase storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -100,25 +100,12 @@ import { PrivacyComponent } from './privacy/privacy.component';
     // "apiKey": "AIzaSyDgZc98sYnuUz2Pb-WALAAI2W8_QSwTOUM",
     //  "authDomain": "dabubble-81b67.firebaseapp.com",
     //   "messagingSenderId": "983219363350" })),
-    PickerComponent,  
-    // provideFirebaseApp(() => initializeApp({ 
-    //   "projectId": "dabubble-81b67", 
-    // "appId": "1:983219363350:web:25eacf514195e328531f37", 
-    // "storageBucket": "dabubble-81b67.appspot.com", 
-    // "apiKey": "AIzaSyDgZc98sYnuUz2Pb-WALAAI2W8_QSwTOUM",
-    //  "authDomain": "dabubble-81b67.firebaseapp.com",
-    //   "messagingSenderId": "983219363350" })),
-    // provideFirestore(() => getFirestore()),
+   
 
     // provideFirebaseApp(() => initializeApp({ "projectId": "dabubble-c4b96", "appId":
     //  "1:390532295885:web:ad6cd75380acc256d50d84", "storageBucket": "dabubble-c4b96.appspot.com", "apiKey": "AIzaSyAEu4ozPAp4fPV6zGVHoY_x9YT8wbZrMs4", 
     //  "authDomain": "dabubble-c4b96.firebaseapp.com", "messagingSenderId": "390532295885" })),
     // provideFirestore(() => getFirestore()),
-
-    provideFirebaseApp(() => initializeApp({ "projectId": "dabubble-c4b96", "appId":
-     "1:390532295885:web:ad6cd75380acc256d50d84", "storageBucket": "dabubble-c4b96.appspot.com", "apiKey": "AIzaSyAEu4ozPAp4fPV6zGVHoY_x9YT8wbZrMs4", 
-     "authDomain": "dabubble-c4b96.firebaseapp.com", "messagingSenderId": "390532295885" })),
-    provideFirestore(() => getFirestore()),
   
     provideFirebaseApp(() => initializeApp({ 
       "projectId": "dabubble-dec6e", 
