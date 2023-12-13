45bdff689db85bef9947dbf7b0f5cce458069c7a
michaelwoelfel <woelfelm@t-online.de>
2023-12-13 16:50:34 +0100

    emoji comments

--- src/app/emoji.service.ts
+++ src/app/emoji.service.ts
@@ -14,29 +14,39 @@ export class EmojiService {
   constructor() { }
 
 
-
+  /**
+   * Adds an emoji to the main chat.
+   * Updates the emojiString with the selected emoji and hides the picker.
+   *
+   */
   addEmojiMainChat(event: any) {
     let emojiString = event["emoji"].native;
     this.emojiString = emojiString;
     this.showMainChatEmojiPicker == false;
   }
 
-
+ /**
+   * Adds an emoji to the thread.
+   * Updates the emojiString with the selected emoji and hides the picker.
+   *
+   */
   addEmojiThread($event: any) {
     let emojiString = $event["emoji"].native;
     this.emojiString = emojiString;
     this.showThreadEmojiPicker == false;
-   
   }
 
+
+/**
+   * Adds an emoji to the text chat.
+   * Updates the emojiString with the selected emoji and hides the picker.
+   *
+   */
   addEmojiTextChat(event: any) {
     let emojiString = event["emoji"].native;
     this.emojiString = emojiString;
     this.showTextChatEmojiPicker = false;
   }
-
-
-
 }
 
 
